9bcee5c8d97de1da8731c22baebab880039af7a2
Dani Dan <dani.dan_95@yahoo.com>
2022-11-02 12:56:24 +0200

    2/Nov/22 - helpful comments added

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
             @keypress="fetchWeatherBySearch"
         />
         <span class="get-my-location" @click="getGeolocation">
-          <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <g id="🔍-Product-Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                   <g id="ic_fluent_my_location_24_regular" fill="#212121" fill-rule="nonzero">
                       <path d="M12,2 C12.3796958,2 12.693491,2.28215388 12.7431534,2.64822944 L12.75,2.75 L12.7490685,4.53770881 L12.7490685,4.53770881 C16.292814,4.88757432 19.1124257,7.70718602 19.4632195,11.2525316 L19.5,11.25 L21.25,11.25 C21.6642136,11.25 22,11.5857864 22,12 C22,12.3796958 21.7178461,12.693491 21.3517706,12.7431534 L21.25,12.75 L19.4616558,12.7490368 L19.4616558,12.7490368 C19.1124257,16.292814 16.292814,19.1124257 12.7474684,19.4632195 L12.75,19.5 L12.75,21.25 C12.75,21.6642136 12.4142136,22 12,22 C11.6203042,22 11.306509,21.7178461 11.2568466,21.3517706 L11.25,21.25 L11.2509632,19.4616558 L11.2509632,19.4616558 C7.70718602,19.1124257 4.88757432,16.292814 4.53678051,12.7474684 L4.5,12.75 L2.75,12.75 C2.33578644,12.75 2,12.4142136 2,12 C2,11.6203042 2.28215388,11.306509 2.64822944,11.2568466 L2.75,11.25 L4.53770881,11.2509315 L4.53770881,11.2509315 C4.88757432,7.70718602 7.70718602,4.88757432 11.2525316,4.53678051 L11.25,4.5 L11.25,2.75 C11.25,2.33578644 11.5857864,2 12,2 Z M12,6 C8.6862915,6 6,8.6862915 6,12 C6,15.3137085 8.6862915,18 12,18 C15.3137085,18 18,15.3137085 18,12 C18,8.6862915 15.3137085,6 12,6 Z M12,8 C14.209139,8 16,9.790861 16,12 C16,14.209139 14.209139,16 12,16 C9.790861,16 8,14.209139 8,12 C8,9.790861 9.790861,8 12,8 Z" id="🎨-Color"></path>
@@ -59,10 +59,12 @@ export default {
   created() {
     let vm = this;
 
+    //Get geolocation on creation of Vue App and on callback get weather for that location.
     vm.getGeolocation();
   },
 
   methods: {
+    //API call for weather by search input
     fetchWeatherBySearch(e) {
       if (e.key === "Enter") {
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
@@ -71,17 +73,20 @@ export default {
             }).then(this.setResults);
       }
     },
+
+    //API call for weather by Geolocation
     fetchWeatherByGeolocation() {
       fetch(`${this.url_base}weather?lat=${this.latitude}&lon=${this.longitude}&limit=5&units=metric&APPID=${this.api_key}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
     },
+
     setResults(results) {
       this.weather = results;
     },
 
-    //
+    //Builds up the date as shown on interface
     dateBuilder() {
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -137,6 +142,8 @@ export default {
 
       return weatherCondition;
     },
+
+    //Calculates temperature on toggle buttons for Celsius or Fahrenheit
     temperatureCalculate() {
       let vm = this,
           temperature = Math.round(vm.weather.main.temp);
